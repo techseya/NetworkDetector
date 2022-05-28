c87f4658731d2e484524b016668383bd02fe10d9
Techseya LK <104070580+techseya@users.noreply.github.com>
2022-05-28 17:47:41 +0530

    Update NetworkDetector.java

--- NetworkDetector/src/main/java/lk/techseya/networkdetector/NetworkDetector.java
+++ NetworkDetector/src/main/java/lk/techseya/networkdetector/NetworkDetector.java
@@ -1,32 +1,66 @@
 package lk.techseya.networkdetector;
 
-import static com.google.android.material.resources.MaterialResources.getDrawable;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableContainer;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.theme.overlay.MaterialThemeOverlay;
+import androidx.annotation.Nullable;
 
-public class NetworkDetector {
-        @SuppressLint({"RestrictedApi", "UseCompatLoadingForDrawables"})
-    public static void Detector(Context context, Activity activity, String title, String message){
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.WanderingCubes;
+import com.github.ybq.android.spinkit.style.Wave;
+
+
+public class NetworkDetector extends Activity {
+    @SuppressLint({"RestrictedApi", "UseCompatLoadingForDrawables"})
+    public static void Detector(Context context, String title, String message){
         ConnectivityManager manager=(ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo anetwork=manager.getActiveNetworkInfo();
-        ProgressDialog progress = new ProgressDialog(activity);
         if (null==anetwork){
-            progress.setTitle(title);
-            progress.setIcon(R.drawable.wifi);
-            progress.setMessage(message);
-            progress.setCancelable(false);
-            progress.show();
+            Dialog dialog = new Dialog(context);
+            dialog.setContentView(R.layout.bg2);
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawableResource(R.drawable.background);
+            ProgressBar progressBar = dialog.findViewById(R.id.spin_kit);
+            Sprite doubleBounce = new Wave();
+            progressBar.setIndeterminateDrawable(doubleBounce);
+            dialog.setCancelable(false);
+            TextView ttitle=dialog.findViewById(R.id.title);
+            TextView tmsg=dialog.findViewById(R.id.message);
+            Button ok=dialog.findViewById(R.id.btn_okay);
+            Button cancel=dialog.findViewById(R.id.btn_cancel);
+            ttitle.setText(title);
+            tmsg.setText(message);
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        context.startActivity(new Intent(android.provider.Settings.Panel.ACTION_INTERNET_CONNECTIVITY));
+                    }
+                }
+            });
+            cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.exit(0);
+                }
+            });
+            dialog.show();
+
+
         }else {
             Toast.makeText(context, "Connected via "+anetwork.getTypeName(), Toast.LENGTH_SHORT).show();
         }
@@ -53,8 +87,4 @@ public class NetworkDetector {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-
-
-
 }
