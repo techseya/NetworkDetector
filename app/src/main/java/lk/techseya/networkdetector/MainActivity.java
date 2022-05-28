4dec17585aab17402ea85e8280776a420470e0a3
Techseya LK <104070580+techseya@users.noreply.github.com>
2022-05-28 18:14:32 +0530

    Update MainActivity.java

--- app/src/main/java/lk/techseya/networkdetector/MainActivity.java
+++ app/src/main/java/lk/techseya/networkdetector/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        NetworkDetector.Detector(this,MainActivity.this,"Searching for Network","Network connection not found...");
+        
     }
 }
